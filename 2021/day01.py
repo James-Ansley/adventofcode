14b5e1a65546be21f95969138d09cac42376b9bc
James Ansley <ansleyjpf@gmail.com>
2021-12-02 08:16:43 +1300

    Update day01.py

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,7 +1,6 @@
 with open('input_files/day01') as f:
-    data = [int(x) for x in f.read().splitlines()]
+    data = [int(x) for x in f.readlines()]
 
 print(sum(x < y for x, y in zip(data, data[1:])))
 
-windowed = [sum(data[i:i + 3]) for i in range(len(data) - 2)]
-print(sum(x < y for x, y in zip(windowed, windowed[1:])))
+print(sum(x < y for x, y in zip(data, data[3:])))
