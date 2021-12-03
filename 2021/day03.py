5d69cbee4193a749e5f69d2c0a3073d8cbc60883
James Ansley <ansleyjpf@gmail.com>
2021-12-03 18:56:27 +1300

    Update day03.py

--- 2021/day03.py
+++ 2021/day03.py
@@ -12,14 +12,10 @@ print(gamma * (2 ** bits - 1 ^ gamma))
 o2, co2 = [*data], [*data]
 for i in range(bits - 1, -1, -1):
     o2_bit = sum((x >> i) & 1 for x in o2) >= len(o2) / 2
-    o2 = [x for x in o2 if (x >> i) & 1 == o2_bit]
-    if len(o2) == 1:
-        break
+    o2 = [x for x in o2 if (x >> i) & 1 == o2_bit] or o2
 
 for i in range(bits - 1, -1, -1):
     co2_bit = sum((x >> i) & 1 for x in co2) < len(co2) / 2
-    co2 = [x for x in co2 if (x >> i) & 1 == co2_bit]
-    if len(co2) == 1:
-        break
+    co2 = [x for x in co2 if (x >> i) & 1 == co2_bit] or co2
 
 print(o2[0] * co2[0])
