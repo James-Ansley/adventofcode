03b212b74556b6bb780b6655a6ff20628759cc26
James Ansley <ansleyjpf@gmail.com>
2021-12-09 00:43:36 +1300

    Update day08.py

--- 2021/day08.py
+++ 2021/day08.py
@@ -2,24 +2,23 @@ import re
 
 with open('input_files/day08') as f:
     data = re.findall(r'(.*) \| (.*)', f.read())
-    data = [(inputs.split(), outputs.split()) for inputs, outputs in data]
-    ins, outs = list(zip(*data))
+    data = [(ins.split(), outs.split()) for ins, outs in data]
 
 uniques = 0
-for outputs in outs:
-    uniques += sum(len(output) in (2, 3, 4, 7) for output in outputs)
+for _, outs in data:
+    uniques += sum(len(output) in (2, 3, 4, 7) for output in outs)
 
 print(uniques)
 
 output_sums = 0
-for inputs, outputs in zip(ins, outs):
+for ins, outs in data:
     digits = [set()] * 10
-    digits[1] = next(set(input_) for input_ in inputs if len(input_) == 2)
-    digits[4] = next(set(input_) for input_ in inputs if len(input_) == 4)
-    digits[7] = next(set(input_) for input_ in inputs if len(input_) == 3)
-    digits[8] = next(set(input_) for input_ in inputs if len(input_) == 7)
+    digits[1] = next(set(in_) for in_ in ins if len(in_) == 2)
+    digits[4] = next(set(in_) for in_ in ins if len(in_) == 4)
+    digits[7] = next(set(in_) for in_ in ins if len(in_) == 3)
+    digits[8] = next(set(in_) for in_ in ins if len(in_) == 7)
 
-    five_lens = (set(input_) for input_ in inputs if len(input_) == 5)
+    five_lens = (set(in_) for in_ in ins if len(in_) == 5)
     for five_len in five_lens:
         if digits[1].issubset(five_len):
             digits[3] = five_len
@@ -27,7 +26,7 @@ for inputs, outputs in zip(ins, outs):
             digits[5] = five_len
         else:
             digits[2] = five_len
-    six_lens = (set(input_) for input_ in inputs if len(input_) == 6)
+    six_lens = (set(in_) for in_ in ins if len(in_) == 6)
     for six_len in six_lens:
         if len(six_len & digits[4]) == 4:
             digits[9] = six_len
@@ -37,8 +36,8 @@ for inputs, outputs in zip(ins, outs):
             digits[0] = six_len
 
     value = 0
-    for output in outputs:
-        value = value * 10 + digits.index(set(output))
+    for out in outs:
+        value = value * 10 + digits.index(set(out))
     output_sums += value
 
 print(output_sums)
