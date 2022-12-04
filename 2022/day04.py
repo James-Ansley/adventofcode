12ed26312cf96f1e8255cb06ef450068b3d659f1
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2022-12-04 18:30:37 +1300

    day4 python

---
+++ 2022/day04.py
@@ -0,0 +1,11 @@
+import re
+
+with open("input_files/day04", "r") as f:
+    data = re.findall("(\d+)-(\d+),(\d+)-(\d+)", f.read())
+    data = [[int(e) for e in row] for row in data]
+
+contained = [(a <= c <= d <= b or c <= a <= b <= d) for a, b, c, d in data]
+overlap = [max(a, c) <= min(b, d) for a, b, c, d in data]
+
+print(sum(contained))
+print(sum(overlap))
