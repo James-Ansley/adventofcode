38cee495182ecc4440b4dc9893af69e935aa690b
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2022-12-06 22:50:30 +1300

    Update day06.py

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,8 +1,8 @@
 with open("input_files/day06", "r") as f:
     data = f.read()
 
-fours = [set(data[i:i+4]) for i in range(len(data))]
-fourteens = [set(data[i:i + 14]) for i in range(len(data))]
+fours = (set(data[i:i+4]) for i in range(len(data)))
+fourteens = (set(data[i:i + 14]) for i in range(len(data)))
 
 idx4 = next(i + 4 for i, window in enumerate(fours) if len(window) == 4)
 idx14 = next(i + 14 for i, window in enumerate(fourteens) if len(window) == 14)
