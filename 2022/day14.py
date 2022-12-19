3a3302bcdb5e1b779281896c85799640058e26ee
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2022-12-19 14:28:26 +1300

    day 14

---
+++ 2022/day14.py
@@ -0,0 +1,43 @@
+import re
+from itertools import count, pairwise, product
+
+
+def fall(sx, sy, grid, lowest_point):
+    if (500, 0) in grid:
+        return True, grid
+    for y in range(sy, lowest_point + 1):
+        if (sx, y) in grid:
+            if (sx - 1, y) not in grid:
+                return fall(sx - 1, y, grid, lowest_point)
+            elif (sx + 1, y) not in grid:
+                return fall(sx + 1, y, grid, lowest_point)
+            else:
+                return False, grid | {(sx, y - 1)}
+    return True, grid
+
+
+def run(grid):
+    lowest_point = max(p[1] for p in grid)
+    for i in count(0):
+        is_stopped, grid = fall(500, 0, grid, lowest_point)
+        if is_stopped:
+            return i
+
+
+with open("input_files/day14", "r") as f:
+    data = f.read().splitlines()
+    data = [re.findall(r"(\d+),(\d+)", line) for line in data]
+    data = [[(int(v1), int(v2)) for v1, v2 in line] for line in data]
+
+points = set()
+for line in data:
+    for start, end in pairwise(line):
+        (x1, x2), (y1, y2) = (sorted(pair) for pair in zip(start, end))
+        points.update(product(range(x1, x2 + 1), range(y1, y2 + 1)))
+
+lowest_point = max(p[1] for p in points) + 2
+left, right = 500 - lowest_point, 501 + lowest_point
+inf_floor = {(i, lowest_point) for i in range(left, right)}
+
+print(run(points))
+print(run(points | inf_floor))
