e3bb5b46ee510787b7457b544fcc22a7a86331a8
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2024-12-06 21:47:02 +1300

    day 06

---
+++ 2024/day06.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from itertools import product
+
+with open("input_files/day06", "r") as f:
+    data = f.read()
+
+start_idx = data.find("^")
+grid = data.splitlines()
+width, height = len(grid[0]), len(grid)
+start = start_idx // (width + 1), start_idx % (width + 1)
+
+
+def run(block=None):
+    seen = defaultdict(set)
+    di, dj = -1, 0
+    i, j = start
+    while 0 <= i < width and 0 <= j < height and (di, dj) not in seen[(i, j)]:
+        seen[(i, j)].add((di, dj))
+        i1, j1 = i + di, j + dj
+        if (0 <= i1 < height and 0 <= j1 < width
+              and (grid[i1][j1] == "#" or (i1, j1) == block)):
+            di, dj = dj, -di
+        else:
+            i, j = i1, j1
+    return len(seen), 0 <= i < width and 0 <= j < height
+
+
+total = 0
+for i, j in product(range(height), range(width)):
+    if grid[i][j] != "#" and (i, j) != start:
+        total += run((i, j))[1]
+
+print(run()[0])
+print(total)
